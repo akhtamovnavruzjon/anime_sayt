2690c2666a5700976f98616b8677d78f8bed1c26
axtamovnavruz <navruzaxtamov1317@gmail.com>
2026-07-14 02:10:45 +0300

    Tamom

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-9*6@q89p49-vhos)i62hg=o1_xdqx7i6zr@t4_dlc-^8tweiv8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
